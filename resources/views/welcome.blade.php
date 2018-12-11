c15902880a77dfcc65c0a2363fa33a494d06b10d
Melvin <mcuadrante020@gmail.com>
2018-12-11 10:25:52 +0800

    initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
       <div class="row">
         <div class="mx-auto col-lg-8 col-md-10">
           <h1 class="display-4 mb-4 text-primary">Welcome to FriendBits!</h1>
-          <p class="lead mb-5">Tell them your story with your friends. LIKE, POST, COMMENT!</p> 
+          <p class="lead mb-5">Tell them your story with your friends. Like, Post and Comment!</p> 
           <a href="{{ route('login') }}" class="btn btn-lg btn-primary mx-1" >Login</a>
           <a href="{{ route('register') }}" class="btn btn-lg btn-outline-primary">Join Now!</a> 
         </div>
