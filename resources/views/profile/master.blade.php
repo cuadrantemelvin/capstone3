ff369ed58662ae3ece318a94204830138288c068
Melvin <mcuadrante020@gmail.com>
2018-12-11 10:18:40 +0800

    initial

--- resources/views/profile/master.blade.php
+++ resources/views/profile/master.blade.php
@@ -32,7 +32,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel bg-primary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                    FriendBit
+                    FriendBits
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
